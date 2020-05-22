8cbb92aa25e890486ec6d71553638836d94f54ae
Wilson <wilsonst.suarez@hotmail.com>
2020-05-22 14:18:53 -0500

    Set on git

---
+++ src/view/Main.java
@@ -0,0 +1,35 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(Main.class.getName());
+
+	}
+	
+	
+	public void settings() {
+	size(500,500);	
+	}
+	
+	
+	public void setup() {
+	
+	}
+	
+	
+	public void draw() {
+	background(0);
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
